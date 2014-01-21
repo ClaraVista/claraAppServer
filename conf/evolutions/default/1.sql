ad2d73057962d85bb54c932ab6742410cb1584fe
claravista <github.claravista@gmail.com>
2014-01-21 18:59:41 +0100

    add user table

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,16 @@
+# Users schema
+
+# --- !Ups
+
+CREATE TABLE User (
+    id bigint(20) NOT NULL AUTO_INCREMENT,
+    login varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    isAdmin boolean NOT NULL,
+    PRIMARY KEY (id)
+);
+
+# --- !Downs
+
+DROP TABLE User;
